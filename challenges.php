fd02db870254f43dcbc1248489002b72b709e4a0
NextGen-Coder <nextgenerationcoder@gmail.com>
2020-02-22 21:53:19 +0530

    Added Compiler

--- challenges.php
+++ challenges.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="bootstrap/jquery/dist/jquery.min.js"></script>
-    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
-    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/bootstrap/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
+    <script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="assets/styles/phase.css">
+    <script src="./assets/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
 </head>
 <style>
     .compiler {
@@ -60,9 +62,34 @@
                         </div>
                     </div>
                     <div class="row ">
-                        <div class="col-sm-8 compiler bg-secondary">
+                        <div class="col-sm-8 compiler">
                             <div class="row">
-                                compiler
+                                <!--Compiler-->
+                                <form class="w-100" id="editorForm" action="controller/CodeController.php" method="post">
+                                    <div id="code-edit" class="row code-div mx-auto">
+                                        <div id="editor-menu">
+                                            <select name="language" class="options" id="prolang">
+                                                <option value="java">Java</option>
+                                                <option value="python">Python</option>
+                                                <option value="c">C</option>
+                                                <option value="cpp">Cpp</option>
+                                                <option value="javascript">Javascript</option>
+                                            </select>
+                                            <select class="options" id="theme">
+                                                <option value="dracula">Dark</option>
+                                                <option value="xcode">Light</option>
+                                            </select>
+                                            <input type="text" hidden name="code" id="hiddencode">
+                                            <button id="screen">
+                                                <img id="screen-img" src="./assets/images/full.png">
+                                            </button>
+                                            <button id="run" type="submit">
+                                                <img id="run-img" src="./assets/images/run2.png">
+                                            </button>
+                                        </div>
+                                        <div id="editor"> </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                         <div class="col-sm-4 ">
@@ -81,5 +108,5 @@
     </div>
     </div>
 </body>
-
+<script src="assets/scripts/compiler.js"></script>
 </html>
