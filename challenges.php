3ea3382b457f70f52e1ffd4a1fdb5897dd5c4bc6
Akramshaha <akramshaha1617@gmail.com>
2020-02-22 23:21:31 +0530

    completed

--- challenges.php
+++ challenges.php
@@ -11,6 +11,11 @@
     <link rel="stylesheet" href="login.css">
 </head>
 <style>
+    .header {
+        padding: 20px;
+        font-size: 20px;
+        font-family: serifs;
+    }
     .compiler {
         margin-top: 10px;
     }
@@ -18,56 +23,82 @@
     .description,
     .compiler {
         width: 100%;
-        height: 100px;
+        height: 200px;
+        border-radius: 5px;
     }
 
     .levels {
-        height: 100%;
+        height: 400px;
+        border-radius: 5px;
+
+    }
+
+    .list-group-item-dark {
+        padding: 25px 0px;
+    }
+    .list-group-item-dark:hover {
+        text-decoration: none;
+    }
+    .blocks {
+        font-size: 20px;
     }
 </style>
 
-<body class="bg-dark text-white ">
-    <div class="" style="height: 100vh;">
+<body class="bg-dark text-white container">
+    <div style="height: 100vh;">
         <div>
             <h3>CODE-RELAY</h3>
             <div class="row ">
 
                 <div class="col-sm-3">
                     <div class="list-group levels bg-secondary text-center">
-                        <h3 class="text-white text-center">LEVELS</h3>
-                        <a href="#" class="list-group-item list-group-item-secondary">level 1</a>
-                        <a href="#" class="list-group-item list-group-item-secondary">level 2</a>
-                        <a href="#" class="list-group-item list-group-item-secondary">level 3</a>
-                        <a href="#" class="list-group-item list-group-item-secondary">level 4</a>
-                        <a href="#" class="list-group-item list-group-item-secondary">level 5</a>
+                        <h3 class="text-white text-center pb-5 pt-5">LEVELS</h3>
+                        <a href="challenges.php?level=1" class="list-group-item list-group-item-dark">level 1</a>
+                        <a href="challenges.php?level=2" class="list-group-item list-group-item-dark">level 2</a>
+                        <a href="challenges.php?level=3" class="list-group-item list-group-item-dark">level 3</a>
+                        <a href="challenges.php?level=4" class="list-group-item list-group-item-dark">level 4</a>
+                        <a href="challenges.php?level=5" class="list-group-item list-group-item-dark">level 5</a>
                     </div>
                 </div>
+                <?php 
+                session_start();
+                $level = $_GET['level'];
+                
+                
+                if($level == $_GET['level']){  
+                $level = $_GET['level']; 
+
+                include("model/config.php");
+                $query = "SELECT *  FROM challenge WHERE level =$level"; 
+                $myquery=mysqli_query($db,$query);
+                while($row = mysqli_fetch_assoc($myquery)){
+                ?>
                 <div class="col-sm-9 ">
                     <div class="row">
                         <div class="col-sm-8 bg-secondary description">
-                            <div class="row">
-                                Description
+                            <div class="row pb-5">
+                                <span class="ml-2 blocks">Description:</span> <br><?php echo $row['description']; ?>
                             </div>
-                            <div class="row">
-                                Input
+                            <div class="row" style="border-top: 1px solid white;">
+                                <span class="ml-2 blocks">Input :</span><br><?php echo $row['input']; ?>
                             </div>
                         </div>
 
                         <div class="col-sm-4 ">
-                            <div class="bg-secondary" style="height: 100px;">
-                                Expected output
+                            <div class="bg-secondary" style="height: 200px; border-radius: 5px;">
+                                <span class="ml-2 blocks">Expected output:</span> <br><span class="ml-4"><?php echo $row['output']; ?></span>
                             </div>
                         </div>
-                    </div>
+                    </div><?php }   } ?>
                     <div class="row ">
                         <div class="col-sm-8 compiler bg-secondary">
                             <div class="row">
-                                compiler
+                                compiler<!-- compiler added here -->
                             </div>
                         </div>
                         <div class="col-sm-4 ">
-                            <div class="bg-secondary compiler" style="height: 100px;">
-                                output
+                            <div class="bg-secondary compiler" style=" width: 100%; height: 200px; ">
+                                <span class="ml-2 blocks">Output</span>
                             </div>
                         </div>
                     </div>
--- model/InitModel.php
+++ model/InitModel.php
@@ -1,8 +1,20 @@
 <?php 
     class InitCodesModel {
-        public function initializeCodes() {
+        public function initializeCodes($level) {
             include("config.php");
             session_start();
+            
+            $query = "SELECT * FROM challenge WHERE levels ='$level'";
+            $result = mysqli_query($db,$query);
+            while($row = mysqli_fetch_assoc($myquery)){
+                $_SESSION['description'] = $row['description'];
+                $_SESSION['input'] = $row['input'];
+                $_SESSION['output'] = $row['output'];
+
+
+                 
+            }
+            echo "<script>window.alert('data added Successfull'); window.location='../challenges.php';</script>";
 
             
         }
