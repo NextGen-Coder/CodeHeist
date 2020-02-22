be819325f3b8756b48721fa603dc2b53eb565a8b
NextGen-Coder <nextgenerationcoder@gmail.com>
2020-02-22 17:52:35 +0530

    Added GitIgnore

--- start.php
+++ start.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="bootstrap/jquery/dist/jquery.min.js"></script>
-    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
-    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- JQuery Kaha Hai be -->
+    <script src="assets/bootstrap/jquery/dist/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
+    <script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="login.css">
 </head>
 <style>
