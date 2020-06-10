00119129d060d240debe035a1e0dbfc61fe4c179
unknown <mohitugemuge322@gmail.com>
2020-06-10 23:01:30 +0530

    cache

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <link rel="stylesheet" href="/assets/styles/fonts.css">
     <link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
     <style>
         body {
@@ -38,12 +38,7 @@
         .submit{
             border-radius: 18px;
         }
-        @font-face {
-            font-family: "Space Age";
-            src: url(assets/fonts/spaceage.ttf);
-        }
         .btnn{
-            font-family: "Space Age";
             font-size: 1.1em;
         }
     </style>
@@ -52,7 +47,7 @@
     <div class="name">
         <img src="assets/images/name.png" alt="">
     </div>
-    <div class="btnn text-white my-5">
+    <div class="btnn space text-white mt-5">
         <form action="controller/LoginController.php" method="POST" class="">
             LOGIN ID <br>
             <input type="text" name="user" class="text" placeholder="LOGIN ID"> <br><br>
@@ -60,11 +55,11 @@
             <input type="password" id="pass" name="pass" class="text" placeholder="PASSWORD"><br>
             <input type="checkbox" id="checkbox" onclick="toggle()"><code class="px-2 text-white">Show password</code>
             <div class="row justify-content-center">
-                <input type="submit" class="mt-4  btn bg-danger text-white submit px-5" value="LOGIN">
+                <input type="submit" class="mt-3  btn bg-danger text-white submit px-5" value="LOGIN">
             </div>
         </form>
     </div>
-    <div class="name text-white">
+    <div class="name text-white mt-3">
         <p class="text-center">
             In association with <br>
             <b>Priyadarshani J.L. College of Engineering</b>
